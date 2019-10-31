d2b7eb4cb7752b5544f04656d5502ab523d5e4fa
mark-marshall <mark.marshallgp@gmail.com>
2019-10-31 17:00:19 +0000

    yearly teammeate endpoint completed

--- routes/yearly.js
+++ routes/yearly.js
@@ -40,7 +40,7 @@ routes.post('/direct', async (req, res) => {
     // loop over the year keys in the retrieved document for driver
     for (let key in driverA[0].career) {
       if (!key.includes('20')) {
-        break;
+        continue;
       }
       // initialize the delta for the year
       delta = 0;
@@ -71,5 +71,62 @@ routes.post('/direct', async (req, res) => {
     });
   }
 });
+
+/*
+[POST] - to /yearly/teammate
+Request: {
+    "target": "HAM",
+}
+Response: {
+    "2015": 8,
+    "2016": 18,
+    "2017": -29,
+    "2018": -20,
+    "2019": -47
+}
+NOTE: 0s are returned for years with no comparison and values are rounded
+for simplicity. Delta is calculated per session per event as an absolute
+lap time delta.
+*/
+routes.post('/teammate', async (req, res) => {
+    const { target } = req.body;
+    try {
+      const targetDriver = await models.Driver.find({ name: target });
+      // initialize a years object to hold annual deltas
+      const years = {};
+      // loop over the year keys in the retrieved document for driver
+      for (let key in targetDriver[0].career) {
+        if (!key.includes('20')) {
+          continue;
+        }
+        // initialize the delta for the year
+        delta = 0;
+        // loop over every session for the year
+        for (i = 0; i < targetDriver[0].career[key].length; i++) {
+          let time = targetDriver[0].career[key];
+          // check whether both driver and teammate have a valid time for the session
+          if (
+            time[i] &&
+            time[i].fl &&
+            time[i].teammate_fl &&
+            time[i].fl !== 'NULL' &&
+            time[i].teammate_fl !== 'NULL'
+          ) {
+            // increment the yearly delta
+            delta += Math.round(time[i].fl - time[i].teammate_fl);
+          }
+        }
+        // save the delta to the key for that year
+        years[key] = delta;
+      }
+      // return the result
+      res.status(200).json(years);
+    } catch (error) {
+      res.status(500).json({
+        message:
+          'Plese input a valid driver for the target key',
+      });
+    }
+  });
 
 module.exports = routes;
